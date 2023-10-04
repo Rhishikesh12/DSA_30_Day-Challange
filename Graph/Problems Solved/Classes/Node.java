5f2ffe56001ffbbc9fbb9c47f738cb54d5225a51
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-10-04 23:24:35 +0530

    push again

---
+++ Graph/Problems Solved/Classes/Node.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class Node {
+    public int first;
+    public int second;
+
+    public Node(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
