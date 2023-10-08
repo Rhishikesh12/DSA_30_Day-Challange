da8dfb325dae5f431e0861cf32f7547241c168d5
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-10-08 11:31:31 +0530

    rotten Oranges added

---
+++ Graph/Problems Solved/rottenOranges.java
@@ -0,0 +1,19 @@
+/**
+ * rottenOranges
+ */
+public class rottenOranges {
+
+    static class Triplet {
+
+    }
+
+    public static void main(String args[]) {
+        int arr[][] = { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } };
+        int rotting = orangesRotting(arr);
+        System.out.println("Minimum Number of Minutes Required " + rotting);
+    }
+
+    private static int orangesRotting(int[][] arr) {
+        return 0;
+    }
+}
